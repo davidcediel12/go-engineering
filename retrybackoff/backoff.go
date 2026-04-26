dec8996b769f85af25c2bb56f9be2a459ed80c85
David Cediel <esdago18@gmail.com>
2026-04-26 09:06:28 -0500

    Add initial backoff implementation

--- retrybackoff/backoff.go
+++ retrybackoff/backoff.go
@@ -1,6 +1,13 @@
 package retrybackoff
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/rand/v2"
+	"time"
+)
 
 type Backoff struct {
 	retries   int
@@ -15,4 +22,31 @@ func New(options ...Option) *Backoff {
 		opt(b)
 	}
 	return b
+}
+
+func (b *Backoff) Do(ctx context.Context, operation func() error) error {
+	retries := 0
+	succeed := false
+	for !succeed && retries <= b.retries {
+		if err := operation(); err != nil {
+			log.Printf("operation failed, retrying: %v", err)
+
+			jitter := time.Duration(rand.Int64N(b.baseDelay.Milliseconds()))
+			delay := b.baseDelay*time.Duration(math.Pow(2, float64(retries))) + jitter
+			log.Printf("Waiting %d ms to perform the operation", delay)
+			select {
+			case <-ctx.Done():
+				return ctx.Err()
+			case <-time.After(delay): // Exponential backoff
+			}
+			retries++
+
+		} else {
+			succeed = true
+		}
+	}
+	if !succeed {
+		return fmt.Errorf("operation failed after %d retries", b.retries)
+	}
+	return nil
 }
