7f8bcc55832af4cac274fd8c14f2553f61ee875f
David Cediel <esdago18@gmail.com>
2026-05-16 11:25:34 -0500

    Add backoff tests

--- retrybackoff/backoff_test.go
+++ retrybackoff/backoff_test.go
@@ -2,6 +2,7 @@ package retrybackoff_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/davidcediel12/go-engineering/retrybackoff"
@@ -10,28 +11,86 @@ import (
 
 func TestDo(t *testing.T) {
 	type testCase struct {
-		backoff       retrybackoff.Backoff
-		argCtx        context.Context
-		argOperation  func() error
-		expectedError error
+		backoff          retrybackoff.Backoff
+		argCtx           context.Context
+		argOperation     func() error
+		expectedErrorIs  error
+		attempts         func() int
+		expectedAttempts int
 	}
 
 	setupTests := map[string]func(t *testing.T) *testCase{
 		"should_not_retry_when_operation_succeed": func(t *testing.T) *testCase {
-
-			retries := 0
+			t.Helper()
+			attempts := 0
 			operation := func() error {
-				if retries > 0 {
-					t.Errorf("expected no retries")
-				}
-				retries++
+				attempts += 1
 				return nil
 			}
 			return &testCase{
-				argOperation:  operation,
-				argCtx:        context.Background(),
-				backoff:       *retrybackoff.New(),
-				expectedError: nil,
+				argOperation:     operation,
+				argCtx:           context.Background(),
+				backoff:          *retrybackoff.New(),
+				expectedErrorIs:  nil,
+				attempts:         func() int { return attempts },
+				expectedAttempts: 1,
+			}
+		},
+		"should_retry_once_and_then_succeed_if_operation_fails": func(t *testing.T) *testCase {
+			t.Helper()
+			attempts := 0
+			operation := func() error {
+				defer func() { attempts += 1 }()
+				if attempts < 1 {
+					return errors.New("exploded")
+				}
+				return nil
+			}
+			return &testCase{
+				argOperation:     operation,
+				argCtx:           context.Background(),
+				backoff:          *retrybackoff.New(),
+				expectedErrorIs:  nil,
+				attempts:         func() int { return attempts },
+				expectedAttempts: 2,
+			}
+		},
+		"should_exhaust_default_retries_and_fail_when_operation_fails_indefenitely": func(t *testing.T) *testCase {
+			t.Helper()
+			expectedError := errors.New("exploded")
+
+			attempts := 0
+			operation := func() error {
+				attempts += 1
+				return expectedError
+
+			}
+			return &testCase{
+				argOperation:     operation,
+				argCtx:           context.Background(),
+				backoff:          *retrybackoff.New(),
+				expectedErrorIs:  expectedError,
+				attempts:         func() int { return attempts },
+				expectedAttempts: retrybackoff.DefaultRetries + 1, // initial attempt  + retries
+			}
+		},
+		"should_exhaust_custom_retries_and_fail_when_operation_fails_indefenitely": func(t *testing.T) *testCase {
+			t.Helper()
+			expectedError := errors.New("exploded")
+
+			attempts := 0
+			operation := func() error {
+				attempts += 1
+				return expectedError
+
+			}
+			return &testCase{
+				argOperation:     operation,
+				argCtx:           context.Background(),
+				backoff:          *retrybackoff.New(retrybackoff.WithRetries(6)),
+				expectedErrorIs:  expectedError,
+				attempts:         func() int { return attempts },
+				expectedAttempts: 7, // initial attempt  + retries
 			}
 		},
 	}
@@ -42,7 +101,8 @@ func TestDo(t *testing.T) {
 			test := setupTest(t)
 
 			err := test.backoff.Do(test.argCtx, test.argOperation)
-			assert.Equal(t, test.expectedError, err)
+			assert.ErrorIs(t, err, test.expectedErrorIs)
+			assert.Equal(t, test.expectedAttempts, test.attempts())
 		})
 	}
 }
